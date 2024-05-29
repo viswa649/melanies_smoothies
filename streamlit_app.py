c27ceab90db1e5109ce86ff5c1e83c6835ad50c2
viswa649 <kasi.viswanadham@kasmodigital.com>
2024-05-29 10:53:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,' + name_on_order+'!', icon="✅")
 
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
 
